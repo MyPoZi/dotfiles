3b5d52ab059d7594d69c652caa8ad23a7fcf1695
MyPoZi <kxx555u.u@gmail.com>
2020-04-02 21:04:52 +0900

    modify shellに対応させる可能性もあるため、-から_に変更

--- zsh/functions/github-issue-open-browser.zsh
+++ zsh/functions/github-issue-open-browser.zsh
@@ -1,7 +1,7 @@
 # (())は算術を表す。0を偽と判定できる。commands[gh]があるか調べ、+であれば1 なければ0を出力
 # [ ] による式と違って、値が0の時に偽値となる
 if (( $+commands[gh] )); then
-    function github-issue-open-browser() {
+    function github_issue_open_browser() {
         local issue_id=$(gh issue list | fzf | awk '{print $1}' )
         if [ -n "$issue_id" ]; then
             gh issue view -w ${issue_id}
@@ -9,5 +9,5 @@ if (( $+commands[gh] )); then
         zle redisplay
     }
 fi
-zle -N github-issue-open-browser
-bindkey "^g^b^i" github-issue-open-browser
+zle -N github_issue_open_browser
+bindkey "^g^b^i" github_issue_open_browser
--- zsh/functions/github-pull-request-checkout.zsh
+++ zsh/functions/github-pull-request-checkout.zsh
@@ -1,6 +1,6 @@
 # ref: https://fromatom.hatenablog.com/entry/2020/03/31/135410
 if (( $+commands[gh] )); then
-    function github-pull-request-checkout() {
+    function github_pull_request_checkout() {
         local select_pr_id=$(gh pr list | fzf | awk '{print $1}' )
         if [ -n "$select_pr_id" ]; then
             BUFFER="gh pr checkout ${select_pr_id}"
@@ -9,5 +9,5 @@ if (( $+commands[gh] )); then
         zle clean-screan
     }
 fi
-zle -N github-pull-request-checkout
-bindkey "^g^p" github-pull-request-checkout
+zle -N github_pull_request_checkout
+bindkey "^g^p" github_pull_request_checkout
