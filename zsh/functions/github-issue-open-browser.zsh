0f81b96ece6ee419fccca36d3efdf5fcc1fb92ab
MyPoZi <kxx555u.u@gmail.com>
2020-04-02 20:24:27 +0900

    add comment

--- zsh/functions/github-issue-open-browser.zsh
+++ zsh/functions/github-issue-open-browser.zsh
@@ -1,4 +1,5 @@
 # (())は算術を表す。0を偽と判定できる。commands[gh]があるか調べ、+であれば1 なければ0を出力
+# [ ] による式と違って、値が0の時に偽値となる
 if (( $+commands[gh] )); then
     function github-issue-open-browser() {
         local issue_id=$(gh issue list | fzf | awk '{print $1}' )
