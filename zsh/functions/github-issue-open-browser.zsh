98c8156d640d16319680d8d253d9a20aebfa0856
MyPoZi <kxx555u.u@gmail.com>
2020-04-02 19:06:50 +0900

    add: issueをBrowserに見に行くのめんどくさかったので、fzfで探索してBrowserで開く

---
+++ zsh/functions/github-issue-open-browser.zsh
@@ -0,0 +1,12 @@
+# (())は算術を表す。0を偽と判定できる。commands[gh]があるか調べ、+であれば1 なければ0を出力
+if (( $+commands[gh] )); then
+    function github-issue-open-browser() {
+        local issue_id=$(gh issue list | fzf | awk '{print $1}' )
+        if [ -n "$issue_id" ]; then
+            gh issue view -w ${issue_id}
+        fi
+        zle redisplay
+    }
+fi
+zle -N github-issue-open-browser
+bindkey "^g^b^i" github-issue-open-browser
